0cd44ac30483247d81e324b6aef03e6015432f38
g <guillame@crowdcomfort.com>
2023-09-22 16:00:47 -0500

    Global: Fixes of small bugs found running tests with python3
    Signed-off-by: g <guillame@crowdcomfort.com>

--- couchbase_ffi/_cinit.py
+++ couchbase_ffi/_cinit.py
@@ -2,13 +2,13 @@ from __future__ import print_function
 import os
 import subprocess
 import re
-
+import functools
 from cffi import FFI
 
 # Globals
 CPP_OUTPUT = os.path.join(os.path.dirname(__file__), "_lcb.h")
-FAKE_INKPATH = os.path.join(os.path.dirname(__file__), 'fakeinc')
-LCB_ROOT = os.environ.get('PYCBC_CFFI_PREFIX', '')
+FAKE_INKPATH = os.path.join(os.path.dirname(__file__), "fakeinc")
+LCB_ROOT = os.environ.get("PYCBC_CFFI_PREFIX", "")
 
 ffi = FFI()
 C = None
@@ -51,16 +51,22 @@ lcb_n1p_synctok_for(lcb_N1QLPARAMS *params, lcb_t instance,
             const void *key, size_t nkey) { return LCB_SUCCESS; }
 """
 
-RX_SHIFT = re.compile(r'(\(?\d+\)?)\s*((?:<<)|(?:>>)|(?:\|))\s*(\(?\d+\)?)')
+RX_SHIFT = re.compile(r"(\(?\d+\)?)\s*((?:<<)|(?:>>)|(?:\|))\s*(\(?\d+\)?)")
 
 
 def shift_replace(m):
-    ss = '{0} {1} {2}'.format(m.group(1), m.group(2), m.group(3))
+    ss = "{0} {1} {2}".format(m.group(1), m.group(2), m.group(3))
     return str(eval(ss))
 
 
+def cmp(a, b):
+    return (a > b) - (a < b)
+
+
 def do_replace_vals(dh, decl):
-    keys = sorted(dh, cmp=lambda x, y: cmp(len(x), len(y)), reverse=True)
+    keys = sorted(
+        dh, key=functools.cmp_to_key(lambda x, y: cmp(len(x), len(y))), reverse=True
+    )
     for k in keys:
         decl = decl.replace(k, str(dh[k]))
     return decl
@@ -76,34 +82,33 @@ def handle_enumvals(defhash, linedecl):
     linedecl = linedecl.strip()
     cur_decls = []
 
-    for decl in linedecl.split(','):
+    for decl in linedecl.split(","):
         if not decl:
             continue
-        if not '=' in decl:
+        if not "=" in decl:
             cur_decls.append(decl)
             continue
 
-        if '{' in decl:
-            preamble, decl = decl.split('{')
+        if "{" in decl:
+            preamble, decl = decl.split("{")
             preamble += "{"
         else:
             preamble = ""
 
-        if '}' in decl:
-            decl, postamble = decl.split('}')
+        if "}" in decl:
+            decl, postamble = decl.split("}")
             postamble = "}" + postamble
         else:
             postamble = ""
 
         # print(decl)
-        name, val = decl.split('=', 1)
+        name, val = decl.split("=", 1)
         name = name.strip()
         val = val.strip()
-        val = val.replace(',', '')
+        val = val.replace(",", "")
         val = do_replace_vals(defhash, val)
 
-        if not name.lower().startswith('lcb'):
-
+        if not name.lower().startswith("lcb"):
             continue
 
         # print("Handling", decl)
@@ -115,26 +120,31 @@ def handle_enumvals(defhash, linedecl):
         except ValueError:
             ival = int(val, 16)
 
-        decl = '{0}={1}'.format(name, str(ival))
+        decl = "{0}={1}".format(name, str(ival))
         defhash[name] = ival
         cur_decls.append(preamble + decl + postamble)
 
-    ret = ','.join(cur_decls)
-    if '}' not in ret:
-        ret += ','
+    ret = ",".join(cur_decls)
+    if "}" not in ret:
+        ret += ","
     return ret
 
 
-CPP_COMMON = ['gcc', '-E',
-              '-I{0}'.format(FAKE_INKPATH), '-I{0}/include'.format(LCB_ROOT),
-              '-std=c89', '-xc']
+CPP_COMMON = [
+    "gcc",
+    "-E",
+    "-I{0}".format(FAKE_INKPATH),
+    "-I{0}/include".format(LCB_ROOT),
+    "-std=c89",
+    "-xc",
+]
 
 
 def get_preprocessed(csrc, extra_options=None):
     options = CPP_COMMON[::]
     if extra_options:
         options += extra_options
-    options += ['-']
+    options += ["-"]
 
     po = subprocess.Popen(options, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
     stdout, _ = po.communicate(csrc)
@@ -142,25 +152,25 @@ def get_preprocessed(csrc, extra_options=None):
         raise ValueError("Bad CPP Input!")
 
     try:
-        return str(stdout, 'utf8').split("\n")
+        return str(stdout, "utf8").split("\n")
     except TypeError:
         return stdout.split("\n")
 
 
 def _exec_cpp():
-    lines = get_preprocessed(CPP_INPUT)
+    lines = get_preprocessed(CPP_INPUT.encode("utf-8"))
     outlines = []
     defhash = {}
 
-    print('couchbase_ffi.. sanitizing declarations for preprocessor')
+    print("couchbase_ffi.. sanitizing declarations for preprocessor")
     for l in lines:
-        if l.startswith('#'):
+        if l.startswith("#"):
             continue
         if not l:
             continue
 
         # Find definitions
-        if '=' in l and '==' not in l:
+        if "=" in l and "==" not in l:
             # Handle enums
             l = handle_enumvals(defhash, l)
 
@@ -174,7 +184,7 @@ def _exec_cpp():
 
 
 def ensure_header():
-    if os.environ.get('PYCBC_CFFI_REGENERATE'):
+    if os.environ.get("PYCBC_CFFI_REGENERATE"):
         do_generate = True
     elif not os.path.exists(CPP_OUTPUT):
         do_generate = True
@@ -193,7 +203,8 @@ def get_handle():
     ensure_header()
 
     ffi.cdef(open(CPP_OUTPUT, "r").read())
-    ffi.cdef(r'''
+    ffi.cdef(
+        r"""
 #define LCB_CMDOBSERVE_F_MASTER_ONLY ...
 #define LCB_RESP_F_FINAL ...
 #define LCB_CNTL_SET ...
@@ -202,12 +213,15 @@ def get_handle():
 #define LCB_CNTL_VBMAP ...
 #define LCB_CMDVIEWQUERY_F_INCLUDE_DOCS ...
 #define LCB_N1P_QUERY_STATEMENT ...
-''')
+"""
+    )
 
-    C = ffi.verify(VERIFY_INPUT,
-                   libraries=['couchbase'],
-                   library_dirs=[os.path.join(LCB_ROOT, 'lib')],
-                   include_dirs=[os.path.join(LCB_ROOT, 'include')],
-                   runtime_library_dirs=[os.path.join(LCB_ROOT, 'lib')])
+    C = ffi.verify(
+        VERIFY_INPUT,
+        libraries=["couchbase"],
+        library_dirs=[os.path.join(LCB_ROOT, "lib")],
+        include_dirs=[os.path.join(LCB_ROOT, "include")],
+        runtime_library_dirs=[os.path.join(LCB_ROOT, "lib")],
+    )
 
     return ffi, C
